ff443caae0a2e9abff0e5b2250ebedb01e197d69
Evee <57734102+eva0519@users.noreply.github.com>
2021-09-29 17:52:12 +0900

    Add files via upload

--- gui_project/pillow_cp_images.py
+++ gui_project/pillow_cp_images.py
@@ -1,18 +1,29 @@
 import time
 from PIL import ImageGrab
 from tkinter import *
+import tkinter.messagebox as msgbox
+import tkinter.ttk as ttk
 
 root = Tk()
-root.title('이미지 연속 캡쳐 프로그램')
-root.geometry('100x100+800+400')
+root.title('image transformer')
+root.geometry('+800+400')
+root.resizable(True,True)
+
+# delay 설정 V
+# 저장 간격 설정
+# 저장 이름 설정
+# 저장 경로 설정
 
 def image_ct():
 
-    # 버튼 클릭 후 대기시간 5초
-    for i in range(5, 0, -1):
-        text.set('{}'.format(i))
-        btn.update()
-        time.sleep(1)
+    # capture delay
+    if delay_combobox.get() == '즉시 시작':
+        pass
+    else:
+        for i in range(int(delay_combobox.get()), 0, -1):
+            text.set('{}'.format(i))
+            btn.update()
+            time.sleep(1)
 
     text.set('캡쳐 중...')
     btn.update()
@@ -20,7 +31,7 @@ def image_ct():
     # 이미지 저장시간 간격 설정
     for i in range(1,7):
         img = ImageGrab.grab()
-        img.save('image{}.png'.format(i))
+        img.save('KYTimage{}.png'.format(i))
         time.sleep(1) # 1초 간격 6장
 
     text.set('캡쳐 완료')
@@ -28,10 +39,44 @@ def image_ct():
     time.sleep(3)
     text.set('캡쳐 시작')
 
+
+# 스트리밍 정의
+def streaming_save():
+    pass
+
+# delay frame
+delay_frame = LabelFrame(root, width=10, text='캡쳐 대기 시간(Sec)')
+delay_frame.pack(fill='x')
+
+# img_catch_delay input combobox
+delay_values = []
+for i in range (0, 31):
+    if i > 0:
+        delay_values.insert(i, i)
+    else:
+        delay_values.append('즉시 시작')
+
+delay_combobox = ttk.Combobox(delay_frame, height=10, values=delay_values, state="readonly")
+delay_combobox.current(0)
+delay_combobox.pack(pady=10)
+
+print(delay_values)
+
+# def btncmd():
+#     print(delay_combobox.get())
+
+# Button(delay_frame, command=btncmd, text='(test) 시간 가져오기 버튼').pack(pady=10)
+
 # 버튼 / 텍스트 바인딩
 text = StringVar()  
 text.set('캡쳐 시작')
-btn = Button(root, textvariable=text, command=image_ct, width=100, height=100)
-btn.pack()
+btn = Button(root, textvariable=text, command=image_ct, width=30)
+btn.pack(fill=BOTH, pady=5, padx=5, expand=True) # expand 를 통해 세로 확장 가능
+
+# 이미지 병합 & 스트리밍 기능
+text_gif = StringVar()
+text_gif.set('움짤 작성')
+btn_gif = Button(root, textvariable=text_gif, command=streaming_save)
+btn_gif.pack(fill=BOTH, pady=5, padx=5, expand=True)
 
 root.mainloop()
